57c45f4cb727008db227be7008918e6f0cda7a5f
apolo-itnet <apolo.itnet@gmail.com>
2025-06-28 13:30:42 +0600

    Portfolio Project - React Router Commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import Router from "./Router/Router.jsx";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+
+Aos.init({
+  duration: 800,
+  easing: "ease-in-out",
+  once: false,
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={Router} />
+  </StrictMode>
+);
